ed41951598848cc0950cf65dd85a0f82d995eecf
jundoopop <jundolpop@gmail.com>
2020-06-21 20:38:34 +0900

    200621 lecture

---
+++ users/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+# Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    # fieldsets = ("Banana", {"fields": ("avatar", "gender", "bio")})
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+    list_display = ("username", "first_name", "last_name", "currency", "superhost")
